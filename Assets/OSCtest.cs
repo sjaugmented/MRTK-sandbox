9189e3fcfce67ae3c93b9a8e9d798c80fe06da66
DarthSethious <sjaugmented@gmail.com>
2020-03-31 11:53:37 -0700

    + OSCtest.cs

---
+++ Assets/OSCtest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCtest : MonoBehaviour
+{
+    OSC osc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        osc = FindObjectOfType<OSC>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendFartOSC()
+    {
+        OscMessage message = new OscMessage();
+        message.address = "/fart";
+        message.values.Add(1);
+        osc.Send(message);
+        Debug.Log("Fart"); //todo remove
+    }
+}
