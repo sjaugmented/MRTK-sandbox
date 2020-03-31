e64d3ba70e515e248d33daa21b93907c54ae0bab
DarthSethious <sjaugmented@gmail.com>
2020-03-31 14:49:32 -0700

    OSC receiving

---
+++ Assets/OSCReceiver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCReceiver : MonoBehaviour
+{
+    OSC osc;
+    Ship ship;
+
+    [SerializeField] string OSCmess = "/1/push1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        osc = FindObjectOfType<OSC>();
+        osc.SetAllMessageHandler(OnReceiveDestShip);
+
+        ship = FindObjectOfType<Ship>();
+
+        Debug.Log(OSCmess);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnReceiveDestShip(OscMessage message)
+    {
+        Debug.Log("message received");
+        ship.DestroyShip();
+    }
+}
--- Assets/OSCtest.cs
+++ Assets/OSCtest.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class OSCtest : MonoBehaviour
 {
     OSC osc;
-    Ship ship;
-
+    
     int messageValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         osc = FindObjectOfType<OSC>();
-        osc.SetAddressHandler("/DestShip", OnReceiveDestShip);
-
-        ship = FindObjectOfType<Ship>();
     }
 
     // Update is called once per frame
@@ -31,10 +27,5 @@ public class OSCtest : MonoBehaviour
         message.values.Add(messageValue++); // ++ makes sure subsequent presses register in OSCulator
         osc.Send(message);
         Debug.Log("Fart"); //todo remove 
-    }
-
-    private void OnReceiveDestShip(OscMessage message)
-    {
-        ship.DestroyShip();
     }
 }
