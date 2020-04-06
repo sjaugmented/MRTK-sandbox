2045eaac3811a5643d516594274ce06b9cda530b
DarthSethious <sjaugmented@gmail.com>
2020-04-06 11:13:04 -0700

    tweaked spell caster and spell position + particles trigger ship explosion and OSC message

---
+++ Assets/SpellCaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCaster : MonoBehaviour
+{
+    [SerializeField] ParticleSystem spellParticle;
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float castThreshold = 1f;
+        float currVelocity = rigidBody.velocity.magnitude;
+
+        Debug.Log(currVelocity); //todo remove
+
+        if (currVelocity >= castThreshold)
+        {
+            Debug.Log("Spell Cast"); //todo remove
+            CastSpell();
+        }
+    }
+
+    private void CastSpell()
+    {
+        spellParticle.Play();
+    }
+}
