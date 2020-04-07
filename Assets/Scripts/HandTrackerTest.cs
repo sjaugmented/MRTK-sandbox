c5ad6cf0018fce0e90ed29b251ced466a43a940c
DarthSethious <sjaugmented@gmail.com>
2020-04-07 11:13:52 -0700

    left and right hand tracking

--- Assets/Scripts/HandTrackerTest.cs
+++ Assets/Scripts/HandTrackerTest.cs
@@ -8,7 +8,7 @@ public class HandTrackerTest : MonoBehaviour
 {
     [SerializeField] float handPosition = 0f;
     public bool handTracking = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +18,23 @@ public class HandTrackerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out MixedRealityPose pose))
+        var rightHandIndex = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out _);
+        var leftHandIndex = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out _);
+
+
+        if (rightHandIndex)
         {
             handTracking = true;
-            Debug.Log("tracking hand");
-        } else
+            Debug.Log("tracking right hand");
+            
+
+        }
+        else if (leftHandIndex)
+        {
+            handTracking = true;
+            Debug.Log("tracking left hand");
+        }
+        else
         {
             handTracking = false;
         }
