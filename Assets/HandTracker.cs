64a8f3c16c57a5b0533dbe9a7d759378e16bf853
DarthSethious <sjaugmented@gmail.com>
2020-04-06 19:13:29 -0700

    I don't know anymore

---
+++ Assets/HandTracker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class HandTracker : MonoBehaviour
+{
+    MixedRealityPose hands;
+    public enum TrackedHandJoint
+    {
+        IndexDistalJoint,
+        IndexTip
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSourceDetected(SourceStateEventData eventData)
+    {
+        var hand = eventData.Controller as IMixedRealityHand;
+        if (hand != null)
+        {
+            if (hand.TryGetJoint(TrackedHandJoint.IndexTip, out MixedRealityPose jointPose)
+            {
+                // ...
+            }
+        }
+    }
+}
