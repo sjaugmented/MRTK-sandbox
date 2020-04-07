905964a74884608c7f1c08c38c23be49e23dc512
DarthSethious <sjaugmented@gmail.com>
2020-04-07 10:35:52 -0700

    script cleanup + HandTrackerTest.cs & Hand Tracker Test Obj

--- Assets/Scripts/HandTrackerTest.cs
+++ Assets/Scripts/HandTrackerTest.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Microsoft.MixedReality.Toolkit.Utilities;
-using Microsoft.MixedReality.Toolkit.Input;
 
-public class HandTracker : MonoBehaviour
+public class HandTrackerTest : MonoBehaviour
 {
-   
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +13,6 @@ public class HandTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-    
 }
