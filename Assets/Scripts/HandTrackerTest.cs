de08b1a5c83e454dab6ef4b6b5bacaf67b2e44aa
DarthSethious <sjaugmented@gmail.com>
2020-04-07 16:52:14 -0700

    refactored/simplified HandTrackerTest - still no velocity

--- Assets/Scripts/HandTrackerTest.cs
+++ Assets/Scripts/HandTrackerTest.cs
@@ -7,57 +7,54 @@ using UnityEngine;
 
 public class HandTrackerTest : MonoBehaviour
 {
-    [SerializeField] float handPosition = 0f;
     [SerializeField] float castThreshold = 0.5f;
-    public bool handTracking = false;
+
+    Vector3 cameraPos;
+    Vector3 prevHandPos;
+    float velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var rightHandIndex = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out _);
-        var leftHandIndex = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out _);
+        MixedRealityPose right;
+        //MixedRealityPose left;
+
+        var rightHand = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out right);
+        //var leftHand = HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out left);
 
 
-        if (rightHandIndex)
+        if (rightHand)
         {
-            handTracking = true;
-            Debug.Log("tracking right hand");
-            
+            Debug.Log("tracking single hand");
 
-        }
-        else if (leftHandIndex)
-        {
-            handTracking = true;
-            Debug.Log("tracking left hand");
+            cameraPos = Camera.main.transform.position;
+            prevHandPos = right.Position;
+            velocity = (right.Position - prevHandPos).magnitude;
+
+            Debug.Log("velocity = " + velocity);
+
         }
         else
         {
-            handTracking = false;
+            return;
         }
+    }
 
-        MixedRealityPose pose;
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out pose))
-        {
-            // This runs if your "try get joint pose" succeeds. If you get into this block, then pose will be defined
-            // Take a look at this
-            // https://microsoft.github.io/MixedRealityToolkit-Unity/api/Microsoft.MixedReality.Toolkit.Utilities.MixedRealityPose.html#Microsoft_MixedReality_Toolkit_Utilities_MixedRealityPose_Position
+    private void TrackHand(MixedRealityPose pose)
+    {
+        // This runs if your "try get joint pose" succeeds. If you get into this block, then pose will be defined
+        // Take a look at this
+        // https://microsoft.github.io/MixedRealityToolkit-Unity/api/Microsoft.MixedReality.Toolkit.Utilities.MixedRealityPose.html#Microsoft_MixedReality_Toolkit_Utilities_MixedRealityPose_Position
 
-            Debug.Log(pose.Position);
-            Debug.Log(pose.Up);
+        //Debug.Log(pose.Position);
+        //Debug.Log(pose.Up);
 
-            Vector3 lastHandPos = pose.Position;
-            var handPosDistance = (pose.Position - lastHandPos);
-            var handVelocity = handPosDistance.magnitude / Time.deltaTime;
-
-            Debug.Log(handVelocity);
-
-            
-        }
+        
     }
 }
