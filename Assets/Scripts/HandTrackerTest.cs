06f3f9d9194cf8eb5ca7919a17866025ff577ee7
DarthSethious <sjaugmented@gmail.com>
2020-04-10 12:54:34 -0700

    + spell book based on number of fingers up?

--- Assets/Scripts/HandTrackerTest.cs
+++ Assets/Scripts/HandTrackerTest.cs
@@ -9,57 +9,104 @@ public class HandTrackerTest : MonoBehaviour
 {
     [Tooltip("Velocity at which spells are cast")] [SerializeField] float castThreshold = 0.5f;
     [Tooltip("Interval between spells cast")] [SerializeField] float castFreq = 0.5f;
-    [Tooltip("GameObject that casts spells")] [SerializeField] GameObject spellCastObj;
-    [SerializeField] GameObject spellHolo;
+    [Header("Casting and Spell prefabs")]
+    [Tooltip("Visual representation of Light spell")] [SerializeField] GameObject lightCaster;
+    [Tooltip("Visual representation of Fire spell")] [SerializeField] GameObject fireCaster;
+    [Tooltip("Visual representation of Water spell")] [SerializeField] GameObject waterCaster;
+    [Tooltip("Visual representation of Wind spell")] [SerializeField] GameObject windCaster;
+    [Tooltip("Visual representation of Earth spell")] [SerializeField] GameObject earthCaster;
+    [Tooltip("Light spell prefab to cast")] [SerializeField] GameObject lightSpell;
+    [Tooltip("Fire spell prefab to cast")] [SerializeField] GameObject fireSpell;
+    [Tooltip("Water spell prefab to cast")] [SerializeField] GameObject waterSpell;
+    [Tooltip("Wind spell prefab to cast")] [SerializeField] GameObject windSpell;
+    [Tooltip("Earth spell prefab to cast")] [SerializeField] GameObject earthSpell;
 
     bool castIsActive = false;
 
     float prevHandCamDist;
-    public float awayVelocity;    // todo remove public
+    float awayVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        spellCastObj.SetActive(false);
+        lightCaster.SetActive(false);
+        fireCaster.SetActive(false);
+        waterCaster.SetActive(false);
+        windCaster.SetActive(false);
+        earthCaster.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MixedRealityPose pose, rightPose, leftPose;
+        ListenForFingers();
 
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out pose) || HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out pose))
+        MixedRealityPose pose;
+
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Any, out pose))
         {
-            // This runs if your "try get joint pose" succeeds. If you get into this block, then pose will be defined
-            // Take a look at this
-            // https://microsoft.github.io/MixedRealityToolkit-Unity/api/Microsoft.MixedReality.Toolkit.Utilities.MixedRealityPose.html#Microsoft_MixedReality_Toolkit_Utilities_MixedRealityPose_Position
-
-            TrackHandVelocity(pose);
-            spellCastObj.SetActive(true);
-        }
-        else if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out rightPose) && HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out leftPose))
-        {
-            // This runs if your "try get joint pose" succeeds. If you get into this block, then pose will be defined
-            // Take a look at this
-            // https://microsoft.github.io/MixedRealityToolkit-Unity/api/Microsoft.MixedReality.Toolkit.Utilities.MixedRealityPose.html#Microsoft_MixedReality_Toolkit_Utilities_MixedRealityPose_Position
-
-            //Debug.Log(pose.Position);
-            //Debug.Log(pose.Up);
-
-            Debug.Log("tracking two index fingers");
-            castIsActive = false;
-            spellCastObj.SetActive(false);
-
-        }
-        else
-        {
-            castIsActive = false;
-            spellCastObj.SetActive(false);
 
         }
     }
 
-    private void TrackHandVelocity(MixedRealityPose pose)
+    private void ListenForFingers()
+    {
+        MixedRealityPose index, middle, ring, pinky, thumb;
+
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out index))
+        {
+            SetCasters(lightCaster);
+            TrackHandVelocity(index, lightSpell);
+        }
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out index) && HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Any, out middle))
+        {
+            SetCasters(fireCaster);
+            TrackHandVelocity(index, fireSpell);
+        }
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out index) && HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Any, out middle) && HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, Handedness.Any, out ring))
+        {
+            SetCasters(waterCaster);
+            TrackHandVelocity(index, waterSpell);
+        }
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out index) && HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Any, out middle) && HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, Handedness.Any, out ring) && HandJointUtils.TryGetJointPose(TrackedHandJoint.PinkyTip, Handedness.Any, out pinky))
+        {
+            SetCasters(windCaster);
+            TrackHandVelocity(index, windSpell);
+        }
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out index) && HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Any, out middle) && HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, Handedness.Any, out ring) && HandJointUtils.TryGetJointPose(TrackedHandJoint.PinkyTip, Handedness.Any, out pinky) && HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Any, out thumb))
+        {
+            SetCasters(earthCaster);
+            TrackHandVelocity(index, earthSpell);
+        }
+
+        /*else if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Both, out rightPose))
+        {
+            Debug.Log("tracking two index fingers");
+            castIsActive = false;
+            lightCaster.SetActive(false);
+
+        }*/
+        else
+        {
+            castIsActive = false;
+            SetCasters(null);
+
+        }
+    }
+
+    private void SetCasters(GameObject trueCaster)
+    {
+        lightCaster.SetActive(true);
+        fireCaster.SetActive(false);
+        waterCaster.SetActive(false);
+        windCaster.SetActive(false);
+        earthCaster.SetActive(false);
+
+        if (trueCaster == null) return;
+        else trueCaster.SetActive(true);
+    }
+
+    private void TrackHandVelocity(MixedRealityPose pose, GameObject spellToCast)
     {
         // tracks velocity of joint away from camera; if greater than castThreshold then cast spell
 
@@ -71,16 +118,16 @@ public class HandTrackerTest : MonoBehaviour
 
         if (awayVelocity >= castThreshold && !castIsActive)
         {
-            CastSpell(pose.Position, Camera.main.transform.rotation, awayVelocity);
+            CastSpell(spellToCast ,pose.Position, Camera.main.transform.rotation, awayVelocity);
 
         }
     }
 
-    private void CastSpell(Vector3 pos, Quaternion rot, float forwardVel)
+    private void CastSpell(GameObject spellToCast, Vector3 pos, Quaternion rot, float forwardVel)
     {
         //spellParticle.Play();
 
-        GameObject holo = Instantiate(spellHolo, pos, rot);
+        GameObject holo = Instantiate(spellToCast, pos, rot);
         StartCoroutine("CastDelay");
     }
 
