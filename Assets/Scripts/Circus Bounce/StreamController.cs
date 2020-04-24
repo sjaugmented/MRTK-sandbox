9ce3efa4609567cbe03638d794e4ce86862d666c
DarthSethious <sjaugmented@gmail.com>
2020-04-24 11:10:31 -0700

    StreamController fixed

--- Assets/Scripts/Circus Bounce/StreamController.cs
+++ Assets/Scripts/Circus Bounce/StreamController.cs
@@ -8,6 +8,7 @@ public class StreamController : MonoBehaviour
     [SerializeField] int valueDMX = 0;
     [SerializeField] string messageOSC = "/test/";
     [SerializeField] float valueOSC = 1f;
+    [SerializeField] ParticleSystem parentStream;
 
     DMXcontroller dmx;
     OSC osc;
@@ -15,14 +16,21 @@ public class StreamController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        dmx = FindObjectOfType<DMXcontroller>();
+        osc = FindObjectOfType<OSC>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        dmx.SetAddress(channelDMX, valueDMX);
-        SendOSCMessage();
+        var emitting = parentStream.emission;
+
+        if (emitting.enabled == false) return;
+        else
+        {
+            dmx.SetAddress(channelDMX, valueDMX);
+            SendOSCMessage();
+        }
     }
 
     private void SendOSCMessage()
