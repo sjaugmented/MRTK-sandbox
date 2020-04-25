bc69a344049e06e2d8641ff2a6427ffa8f3f753d
DarthSethious <sjaugmented@gmail.com>
2020-04-25 10:17:19 -0700

    created fresh sandbox for finger/palm testing

---
+++ Assets/Scripts/Tests/TrackerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerTest : MonoBehaviour
+{
+    MixedRealityPose indexTip1, indexTip2, palm1, palm2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Any, out indexTip1))
+        {
+            Debug.Log(indexTip1.Forward.z);
+        }
+
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Any, out palm1))
+        {
+            Debug.Log(palm1);
+        }
+    }
+}
